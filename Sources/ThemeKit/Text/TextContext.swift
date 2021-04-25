ec659d98583b332a2a913d8d35134e5b437a58ba
Jacob <>
2021-04-25 12:38:30 +0100

    Use text context instead of UIFont.TextStyle for more flexibility

---
+++ Sources/ThemeKit/Text/TextContext.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jacob Whitehead on 25/04/2021.
+//
+
+import UIKit
+
+public enum TextContext: String, CaseIterable {
+    case body
+    case callout
+    case caption1
+    case caption2
+    case footnote
+    case headline
+    case subheadline
+    case largeTitle
+    case title1
+    case title2
+    case title3
+}
--- Sources/ThemeKit/Theme.swift
+++ Sources/ThemeKit/Theme.swift
@@ -15,11 +15,17 @@ public protocol UIConstantProvider {
     func constant(for context: UIConstantContext) -> CGFloat
 }
 
+public protocol FontProvider {
+    func font(for context: TextContext) -> UIFont
+}
+
 public class Theme {
     
     private static var colorProvider: ColorProvider?
     private static var constantProvider: UIConstantProvider?
+    private static var fontProvider: FontProvider?
     
+    // MARK: - Providers
     public static func color(for context: ColorContext) -> UIColor {
         colorProvider?.color(for: context) ?? .black
     }
@@ -28,12 +34,21 @@ public class Theme {
         constantProvider?.constant(for: context) ?? 0
     }
     
+    public static func font(for context: TextContext) -> UIFont {
+        fontProvider?.font(for: context) ?? .preferredFont(forTextStyle: .body)
+    }
+    
+    // MARK: - Setters
     public static func setColorProvider(_ provider: ColorProvider) {
         colorProvider = provider
     }
     
     public static func setUIConstantProvider(_ provider: UIConstantProvider) {
         constantProvider = provider
+    }
+    
+    public static func setFontProvider(_ provider: FontProvider) {
+        fontProvider = provider
     }
     
 }
